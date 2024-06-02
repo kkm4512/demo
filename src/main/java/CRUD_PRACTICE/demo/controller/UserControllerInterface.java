17515763b0a48710a030c380ec93e103d3a45c57
kkm4512 <nayoun440@naver.com>
2024-06-02 09:37:30 +0900

    updateMap까지 마쳤고, 매번 Test코드에서 user1,user2 객체 생성하는거 귀찮은데 @BeforeEach에 때려박아보기

--- src/main/java/CRUD_PRACTICE/demo/controller/UserControllerInterface.java
+++ src/main/java/CRUD_PRACTICE/demo/controller/UserControllerInterface.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 /**
  * 1. controller에서 구현할 함수들의 interface 설정
- *      [ C R U D ]
+ * [ C R U D ]
  * 1. C //유저 생성
  * 2  R //유저목록 읽기
  * 3. U // 유저정보 업데이트
@@ -19,13 +19,19 @@ public interface UserControllerInterface {
     void create(UserInfo userInfo);
 
     UserInfo updateArray(Long id, UserInfo userInfo);
+
     boolean deleteArray(UserInfo userInfo);
+
     List<UserInfo> readList();
+
     int findOneUserArray(Long id);
 
     UserInfo updateMap(Long id, UserInfo userInfo);
+
     boolean deleteMap(UserInfo userInfo);
-    Map<Long,UserInfo> readMap();
+
+    Map<Long, UserInfo> readMap();
+
     UserInfo findOneUserMap(Long id);
 
 }
