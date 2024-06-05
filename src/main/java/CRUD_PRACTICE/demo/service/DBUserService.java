07a9113d81818d68f9580bc87dbd6f20af691b34
kkm4512 <nayoun440@naver.com>
2024-06-06 00:00:55 +0900

    succced : mysql db connecting success and create function created success
    todo :
    1) 나머지 함수들 만들기
    2) 프론트에 오류 던질때, 내가 설정한 오류가 던져지게 하기 -> 근데 이거 내가볼때 엔티티만 껐다 켜졌을떄 내가 설정한 엔티티로 재반영만 하게하면 될거같음

--- src/main/java/CRUD_PRACTICE/demo/service/DBUserService.java
+++ src/main/java/CRUD_PRACTICE/demo/service/DBUserService.java
@@ -1,13 +1,95 @@
 package CRUD_PRACTICE.demo.service;
 
+import CRUD_PRACTICE.demo.constructor.UserInfo;
+import CRUD_PRACTICE.demo.entity.UserEntity;
 import CRUD_PRACTICE.demo.repository.DBUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
-public class DBUserService {
-    private final DBUserRepository DBuserRepository;
+public class DBUserService implements DBUserServiceInterface{
+    private final DBUserRepository dbUserRepository;
 
-    
+    public static class DuplicateUserException extends RuntimeException {
+        public DuplicateUserException(String msg) {
+            super(msg);
+        }
+    }
+
+    //공백인지 확인
+    public void userCheck(UserEntity user){
+        if ( user.getName() == null || user.getNickname() == null || user.getPassword() == null || user.getEmail() == null) {
+            throw new IllegalArgumentException("사용자의 정보는 공백일 수 없습니다.");
+        }
+    }
+
+//    public void userDuplicateCheck(UserEntity user){
+//        if (dbUserRepository.findByEmail(user.getEmail()).isPresent()) {
+//            throw new DuplicateUserException("이메일이 중복됩니다.");
+//        }
+//        if (dbUserRepository.findByNickname(user.getNickname()).isPresent()) {
+//            throw new DuplicateUserException("닉네임이 중복됩니다.");
+//        }
+//        if (dbUserRepository.findByName(user.getName()).isPresent()) {
+//            throw new DuplicateUserException("이름이 중복됩니다.");
+//        }
+//    }
+
+
+    @Override
+    public boolean create(UserEntity user) {
+        userCheck(user);
+//        userDuplicateCheck(user);
+        dbUserRepository.save(user);
+        return true;
+    }
+
+    @Override
+    public UserEntity updateArray(Long id, UserEntity user) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteArray(Long id) {
+        return false;
+    }
+
+    @Override
+    public List<UserEntity> readList() {
+        return List.of();
+    }
+
+    @Override
+    public int findOneUserIndexArray(Long id) {
+        return 0;
+    }
+
+    @Override
+    public UserEntity findOneUserArray(Long id) {
+        return null;
+    }
+
+    @Override
+    public UserEntity updateMap(Long id, UserEntity user) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteMap(Long id) {
+        return false;
+    }
+
+    @Override
+    public Map<Long, UserEntity> readMap() {
+        return Map.of();
+    }
+
+    @Override
+    public UserEntity findOneUserMap(Long id) {
+        return null;
+    }
 }
