9d8386872044f05d00b58f244b9200be0d543ae4
kkm4512 <nayoun440@naver.com>
2024-06-06 17:51:55 +0900

    Test Code 작성

---
+++ src/test/java/CRUD_PRACTICE/demo/service/DBUserServiceTest.java
@@ -0,0 +1,48 @@
+package CRUD_PRACTICE.demo.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DBUserServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void updateArray() {
+    }
+
+    @Test
+    void deleteArray() {
+    }
+
+    @Test
+    void readList() {
+    }
+
+    @Test
+    void findOneUserIndexArray() {
+    }
+
+    @Test
+    void findOneUserArray() {
+    }
+
+    @Test
+    void updateMap() {
+    }
+
+    @Test
+    void deleteMap() {
+    }
+
+    @Test
+    void readMap() {
+    }
+
+    @Test
+    void findOneUserMap() {
+    }
+}
